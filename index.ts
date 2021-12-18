0211364f547e57fdc99af3387f6f7befebab2f99
sairaj <sairaj2119@gmail.com>
2021-12-18 22:47:17 +0530

    change the splitChar on different os's

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
+import os from 'os'
+
 export type Logger = (msg: string) => void
+
+const splitChar = os.platform() === 'win32' ? '\\' : '/'
 
 export const logger: Logger = (msg: string): void => {
   try {
-    throw new Error(msg)
+    throw new Error()
   } catch (err: any) {
     const stack = err.stack
     let fileTrace = stack.split('\n').slice(1)[1].trim()
-    const splitArr = fileTrace.split('/')
+    const splitArr = fileTrace.split(splitChar)
     const fileName = splitArr[splitArr.length - 1]
     const final = fileName.split(':').splice(0, 2).join(':')
     console.log(`${final} => ${msg}`)
